5d7410a813eff7a094f605fe6da10fbcc1e29b1a
buildLittleWorlds <dplate@lindenwood.edu>
2023-12-17 19:07:53 -0600

    finished core blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://51llms.netlify.app/"
+  site: "https://51llms.netlify.app/",
+  integrations: [preact()]
 });
